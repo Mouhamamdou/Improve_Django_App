c97d3e69d1fb1f2cdb6bf48f289e7a05ccd42ca7
Mouhamadou Gaye <moctargaye94@gmail.com>
2024-07-01 12:31:31 +0200

    Changes to be committed:

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+import profiles.views
+import lettings.views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('', lettings.views.index, name='index'),
+    path('lettings/', lettings.views.lettings_index, name='lettings_index'),
+    path('lettings/<int:letting_id>/', lettings.views.letting, name='letting'),
+    path('profiles/', profiles.views.profiles_index, name='profiles_index'),
+    path('profiles/<str:username>/', profiles.views.profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
